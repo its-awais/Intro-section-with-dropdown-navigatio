1c61e4a31d01b8f4ab22d18335a140f4839df572
its-awais <awaisahmed123334444@gmail.com>
2025-01-18 18:20:28 +0500

    Removed submodule and added as regular directory

---
+++ src/Component/into section dropdown main/IntroMain.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./introMain.css"
+import heroImage from "../../assets/images/image-hero-desktop.png"
+// import partner from "../../assets/images/icon-arrow-down.svg"
+const IntroMain = () => {
+  return (
+    <>
+    <main>
+      <div className="first-column d-flex flex-column justify-content-center">
+      <h1>Make <span className="jan">remote work</span></h1>
+      <p className='content'>Get your team in sync. no matter
+      your location. <br className='something'/>Streamine processes. create team rituals. and <br/>watch productivity soar
+      </p>
+      <button className='learn-more'>Learn more</button>
+    </div>
+    <div className="second-column">
+      <img className='hero-image' src={heroImage} alt="remote work" />
+    </div>
+    {/* <img src={partner} alt="we meant for each other" /> */}
+    </main>
+    </>
+  )
+}
+
+export default IntroMain
+
